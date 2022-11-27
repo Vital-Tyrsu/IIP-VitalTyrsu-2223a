9aecd9283d95aab21008cc564b29b681e40bc7d5
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-11-27 19:51:29 +0100

    commit

---
+++ SlnLes02VariabelenBerekeningen/ConsoleBmi/ConsoleBmi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBmi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("BMI CALCULATOR");
+            Console.WriteLine("==============");
+
+            Console.Write("Lengte (in cm): ");
+            double lengte = Convert.ToInt32(Console.ReadLine());
+            lengte = lengte / 100;
+            Console.Write("Gewicht (in kg): ");
+            int gewicht = Convert.ToInt32(Console.ReadLine());
+
+            double totaal = Math.Round(Convert.ToDouble(gewicht) / (lengte * lengte), 2);
+
+            Console.Write("Je BMI bedraagt: " + totaal);
+
+            Console.ReadLine();
+
+        }
+    }
+}
