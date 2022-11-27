c7cbc9912af840080cd259da949200197e607347
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-11-27 19:55:55 +0100

    commit

---
+++ SlnLes02VariabelenBerekeningen/WpfBMI/WpfBMI/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBMI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Hier maak je gebruik van een double zodat je de cijfers na de komma kan lezen. 
+            double lengte = Convert.ToInt32(lblLenght.Text);
+            lengte = lengte / 100;
+            int gewicht = Convert.ToInt32(lblGewicht.Text);
+
+            double totaal = Math.Round(Convert.ToDouble(gewicht / (lengte * lengte)), 2);
+            lblBMI.Content = ($"BMI : {totaal}");
+
+        }
+    }
+}
