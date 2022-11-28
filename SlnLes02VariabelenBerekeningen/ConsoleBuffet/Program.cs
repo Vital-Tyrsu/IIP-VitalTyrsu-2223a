e1c047e24ac44682f360ca76c6b9783cd9185b4c
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-11-28 17:29:14 +0100

    commit

--- SlnLes02VariabelenBerekeningen/ConsoleBuffet/Program.cs
+++ SlnLes02VariabelenBerekeningen/ConsoleBuffet/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleBuffet
     {
         static void Main(string[] args)
         {
+            char euro = Convert.ToChar('\u20AC');
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(@"WELKOM IN HET BUFFETRESTAURANT
+==============================
+
+Alles is à volonté en dranken zijn inbegrepen!
+!!Promotie!! Vandaag ronden we het totaalbedrag af naar beneden!");
+
+            const double SEIZOENBUFFET = 18.5;
+            const double DESSERTENBUFFET = 7.95;
+            const double KINDERMENU = 8.95;
+
+            Console.WriteLine($"Aantal seizoensbuffets ({euro}18.5):");
+            
+            Console.WriteLine("Fooi (0 indien geen): ");
+            
+
+            Console.ReadLine();
         }
     }
 }
