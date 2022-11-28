f6b9494385d8a9e67ec859cf80622e775775672b
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-11-28 16:36:34 +0100

    commit

---
+++ SlnLes02VariabelenBerekeningen/ConsoleBmi/ConsoleSeconden/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSeconden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geef het aantal seconden: ");
+            int seconden = Convert.ToInt32(Console.ReadLine());
+
+            int dagen = seconden / (24 * 60 * 60);
+            int restseconden = seconden % (24 * 60 * 60);
+
+            int uren = restseconden / (60 * 60);
+            restseconden = restseconden % (60 * 60);
+
+            int minuten = restseconden / 60;
+            restseconden = restseconden % 60;
+
+            int seconds = restseconden;
+
+            Console.WriteLine($"Dit zijn {dagen} en {uren} en {minuten} en {seconds}");
+            Console.ReadLine();
+        }
+    }
+}
