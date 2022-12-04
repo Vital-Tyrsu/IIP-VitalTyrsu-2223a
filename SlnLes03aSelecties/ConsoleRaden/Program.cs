527deaf1f84beab30fe7f8801ffa05421e999ad3
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-12-04 15:46:49 +0100

    commit

---
+++ SlnLes03aSelecties/ConsoleRaden/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRaden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int getal = 7;
+            Console.Write("Raad een getal tussen 1 en 10: ");
+            getal = Convert.ToInt32(Console.ReadLine());
+
+            if (getal > 7)
+            {
+                Console.WriteLine("De gok was te hoog");
+            }
+            else if (getal < 7)
+            {
+                Console.WriteLine("De gok was te laag");
+            }
+            else
+            {
+                Console.WriteLine("Je gok is correct!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SlnLes03aSelecties/WpfSliderKleur/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSliderKleur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sldKleur_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lblGetal.Content = sldKleur.Value;
+
+            if (sldKleur.Value <= 25)
+            {
+                lblGetal.Foreground = Brushes.Green;
+            }
+            else if (sldKleur.Value <=50)
+            {
+                lblGetal.Foreground = Brushes.Yellow;
+            }
+            else if (sldKleur.Value <=75)
+            {
+                lblGetal.Foreground = Brushes.Orange;
+            }
+            else
+            {
+                lblGetal.Foreground= Brushes.Purple;
+            }
+        }
+    }
+}
