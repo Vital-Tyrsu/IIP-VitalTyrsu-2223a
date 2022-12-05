7b569d4ea75c920db35d5df917f47d63413ec659
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-12-05 12:28:17 +0100

    commit

---
+++ SlnLes03aSelecties/ConsoleBestelling/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBestelling
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double prijs = 0;
+
+            Console.WriteLine("PIZZA BESTELLING");
+            Console.WriteLine("================");
+
+            Console.WriteLine(@"Kies je pizza: 
+a) Margherita 8 euro
+b) Funghi 10 euro
+c) Diabolo 11 euro
+>> wat is je keuze? ");
+            char keuze = Convert.ToChar(Console.ReadLine());
+            string pizzaKeuze = "";
+            switch (keuze)
+            {
+                case 'a': pizzaKeuze = "Margherita"; prijs = 8;
+                    break;
+                case 'b': pizzaKeuze = "Funghi"; prijs = 10;
+                    break;
+                case 'c': pizzaKeuze = "Diabolo"; prijs = 11;
+                    break;
+                default:
+                    Console.WriteLine("Ongeldige keuze");
+                    break;
+            }
+
+            Console.WriteLine(@"Kies de grootte:
+a) 15 cm (klein; - 20%)
+b) 20 cm (normaal)
+c) 25 cm (groot; + 20%)
+>> wat is je keuze? ");
+            char grootte = Convert.ToChar(Console.ReadLine());
+            string pizzaGrootte ="";
+            switch (grootte)
+            {
+                case 'a':
+                    pizzaGrootte = "klein"; prijs = prijs * 0.8;
+                    break;
+                case 'b':
+                    pizzaGrootte = "normaal";
+                    break;
+                case 'c':
+                    pizzaGrootte = "groot"; prijs *= 1.2;
+                    break;
+                default:
+                    Console.WriteLine("Ongeldige keuze");
+                    break;
+            }
+
+            Console.WriteLine("Thuis bezorgen (3 euro extra)? ja/nee: ");
+            string extra = Console.ReadLine();
+            if (extra == "ja")
+            {
+                extra = "thuis bezorgd";
+                prijs = prijs + 3;
+            }
+            else
+            {
+                extra = "niet thuis bezorgd";
+            }
+
+
+            Console.WriteLine($"Jouw bestelling: 1 pizza {pizzaKeuze} {pizzaGrootte}, voor {prijs} euro,{extra}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SlnLes03aSelecties/ConsoleEindcijfer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEindcijfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berekening eindcijfer");
+            Console.WriteLine("=====================");
+
+            Console.Write("Geef het cijfer dagelijks werk (op 20): ");
+            double dagelijksWerkCijfer = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geef het cijfer op het project (op 20): ");
+            double projectCijfer = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Geef het cijfer op het examen (op 20): ");
+            double examenCijfer = Convert.ToDouble(Console.ReadLine());
+
+            double eindCijfer = Math.Round((dagelijksWerkCijfer * 0.3) + (projectCijfer * 0.2) + (examenCijfer * 0.5), 1) * 5;
+            Console.WriteLine($"Je eindcijfer is {eindCijfer}%");
+
+            if (eindCijfer > 82.5)
+            {
+                Console.WriteLine("-> grootste onderscheiding");
+            }
+            else if (eindCijfer > 75)
+            {
+                Console.WriteLine("-> grote onderscheiding");
+            }
+            else if (eindCijfer > 67.5)
+            {
+                Console.WriteLine("-> onderscheiding");
+            }
+            else if (eindCijfer > 50)
+            {
+                Console.WriteLine("Voldoende");
+            }
+            else
+            {
+                Console.WriteLine("Onvoldoende");
+            }
+
+            if (examenCijfer < 8)
+            {
+                eindCijfer = Math.Min(eindCijfer, examenCijfer);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
