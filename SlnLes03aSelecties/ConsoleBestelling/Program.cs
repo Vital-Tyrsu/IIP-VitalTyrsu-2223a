a77164cd4395c55cb99a789edf47223f584ff496
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-12-07 18:25:44 +0100

    commit

--- SlnLes03aSelecties/ConsoleBestelling/Program.cs
+++ SlnLes03aSelecties/ConsoleBestelling/Program.cs
@@ -20,9 +20,10 @@ namespace ConsoleBestelling
 a) Margherita 8 euro
 b) Funghi 10 euro
 c) Diabolo 11 euro
+d) 4 seizoenen 13 euro
 >> wat is je keuze? ");
             char keuze = Convert.ToChar(Console.ReadLine());
-            string pizzaKeuze = "";
+            string pizzaKeuze = " ";
             switch (keuze)
             {
                 case 'a': pizzaKeuze = "Margherita"; prijs = 8;
@@ -30,6 +31,8 @@ c) Diabolo 11 euro
                 case 'b': pizzaKeuze = "Funghi"; prijs = 10;
                     break;
                 case 'c': pizzaKeuze = "Diabolo"; prijs = 11;
+                    break;
+                case 'd': pizzaKeuze = "4 seizoenen"; prijs = 13;
                     break;
                 default:
                     Console.WriteLine("Ongeldige keuze");
@@ -40,9 +43,10 @@ c) Diabolo 11 euro
 a) 15 cm (klein; - 20%)
 b) 20 cm (normaal)
 c) 25 cm (groot; + 20%)
+d) 45 cm (Big King XXL; +50%)
 >> wat is je keuze? ");
             char grootte = Convert.ToChar(Console.ReadLine());
-            string pizzaGrootte ="";
+            string pizzaGrootte = " ";
             switch (grootte)
             {
                 case 'a':
@@ -54,25 +58,33 @@ c) 25 cm (groot; + 20%)
                 case 'c':
                     pizzaGrootte = "groot"; prijs *= 1.2;
                     break;
+                case 'd':
+                    pizzaGrootte = "Big King XXL"; prijs *= 1.7;
+                    break;
                 default:
                     Console.WriteLine("Ongeldige keuze");
                     break;
             }
 
-            Console.WriteLine("Thuis bezorgen (3 euro extra)? ja/nee: ");
-            string extra = Console.ReadLine();
-            if (extra == "ja")
+            Console.Write("Thuis bezorgen (3 euro extra)? ja/nee: ");
+            string levering = Console.ReadLine();
+            if (levering == "ja")
             {
-                extra = "thuis bezorgd";
+                levering = "thuis bezorgd";
                 prijs = prijs + 3;
             }
             else
             {
-                extra = "niet thuis bezorgd";
+                levering = "niet thuis bezorgd";
             }
 
-
-            Console.WriteLine($"Jouw bestelling: 1 pizza {pizzaKeuze} {pizzaGrootte}, voor {prijs} euro,{extra}");
+            Console.Write("Vandaag EXTRA korting ! Wens je deze nu te gebruiken of later? ja/nee: ");
+            string extra = Console.ReadLine();
+            if ( extra == "ja")
+            {
+                prijs *= 0.8;
+            }
+            Console.WriteLine($"Jouw bestelling: 1 pizza {pizzaKeuze} {pizzaGrootte}, voor {prijs} euro,{levering}");
             Console.ReadLine();
 
         }
