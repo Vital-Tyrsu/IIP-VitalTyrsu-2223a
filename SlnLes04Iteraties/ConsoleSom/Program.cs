6ef6ebb3119f9b89e0e7c59f84c080c2c51785e9
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2022-12-12 14:51:55 +0100

    commit

---
+++ SlnLes04Iteraties/ConsoleSom/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int som = 0;
+            
+            do
+            {
+                Console.Write("Voer een getal in (q om te stoppen): ");
+                input = Convert.ToString(Console.ReadLine());
+                if (input != "q")
+                {
+                   int getal = Convert.ToInt32(input);
+                   som = som + getal;
+                }
+                
+
+            } while (input != "q");
+            Console.WriteLine("De som is: " + som);
+
+            Console.ReadLine();
+        }
+    }
+}
