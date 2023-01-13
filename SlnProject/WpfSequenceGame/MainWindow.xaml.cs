17b492cd38eeeee7bd8bf9bca038a70efe251ba6
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2023-01-13 11:01:26 +0100

    commit

--- SlnProject/WpfSequenceGame/MainWindow.xaml.cs
+++ SlnProject/WpfSequenceGame/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Media;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,10 +22,76 @@ namespace WpfSequenceGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random = new Random();
+        List<int> pattern = new List<int>();
+        int round = 1;
+
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
-            
+            TxtNaam.Text = Console.ReadLine();
+            int numRounds = Convert.ToInt32(TxtRounds.Text);
+
+            while (round <= numRounds)
+            {
+                pattern.Add(random.Next(1, 5));
+                foreach (int color in pattern)
+                {
+                    TxtSequencePattern.Text = (getAnimalName(color) + " ");
+                    System.Threading.Thread.Sleep(2000);
+                }
+
+                List<int> input = new List<int>();
+                while (input.Count < pattern.Count)
+                {
+                    string key = Console.ReadLine();
+                    if (key == "1")
+                    {
+                        input.Add(1);
+                    }
+                    else if (key == "2")
+                    {
+                        input.Add(2);
+                    }
+                    else if (key == "3")
+                    {
+                        input.Add(3);
+                    }
+                    else if (key == "4")
+                    {
+                        input.Add(4);
+                    }
+                }
+            }
         }
 
+
+        static string playSound(string sound)
+        {
+            string path = System.IO.Path.Combine(Environment.CurrentDirectory, sound);
+            SoundPlayer player = new SoundPlayer(path);
+            player.Play();
+            return " ";
+        }
+
+        // Methode
+        static string getAnimalName(int keuze)
+        {
+            if (keuze == 1)
+            {
+                return playSound("sounds/hond.wav");
+            }
+            else if (keuze == 2)
+            {
+                return playSound("sounds/koe.wav");
+            }
+            else if (keuze == 3)
+            {
+                return playSound("sounds/kuikentje.wav");
+            }
+            else
+            {
+                return playSound("sounds/paard.wav");
+            }
+        }
     }
 }
