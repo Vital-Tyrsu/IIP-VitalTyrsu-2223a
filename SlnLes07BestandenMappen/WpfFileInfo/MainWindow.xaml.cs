6ef2f81961a1bf1188d833a5cf5d76d7de4ae101
Vital-Tyrsu <78963164+Vital-Tyrsu@users.noreply.github.com>
2023-01-24 16:42:36 +0100

    commit

---
+++ SlnLes07BestandenMappen/WpfFileInfo/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfFileInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                string fileText = File.ReadAllText(filePath);
+                string lastModifiedDate = File.GetLastWriteTime(filePath).ToString("MM/dd/yyyy hh:mm:ss tt");
+                string[] words = fileText.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                int wordCount = words.Length;
+                lblFilePath.Content = "bestandsnaam" + filePath;
+                lblWordCount.Content = "aantal woorden" + wordCount;
+                lblFileLaatstaangemaakt.Content = "gemaakt op" + lastModifiedDate;
+            }
+        }
+
+    }
+}
